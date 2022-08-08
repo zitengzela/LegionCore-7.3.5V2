a40df84b4f20d6c0039bcb075582449598335633
SargeroDeV <neohd251535@gmail.com>
2022-08-08 16:59:49 -0500

    DB: Add game_tele of class halls.

--- sql/updates/world/2022_08_01_01_world.sql
+++ sql/updates/world/2022_08_01_01_world.sql
@@ -2,6 +2,5 @@ UPDATE `creature_template` SET `npcflag`= npcflag | 1 WHERE `entry` IN (108767,1
 
 UPDATE `creature_template` SET `InhabitType`= 4 WHERE `entry` IN (105172,91951);
 
-UPDATE `creature_template` SET `InhabitType`= 3 WHERE `entry` IN (100395,110628,110627,91949,91353,101747,101713,101708);
 
 
