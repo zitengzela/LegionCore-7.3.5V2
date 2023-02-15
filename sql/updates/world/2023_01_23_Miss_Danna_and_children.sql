e5b7e2870957f13098988f7680173b33d4b178ac
Devir7 <97642031+Devir7@users.noreply.github.com>
2023-02-15 07:16:38 +0600

    Update 2023_01_23_Miss_Danna_and_children.sql
    Signed-off-by: Devir7 <97642031+Devir7@users.noreply.github.com>

--- sql/updates/world/2023_01_23_Miss_Danna_and_children.sql
+++ sql/updates/world/2023_01_23_Miss_Danna_and_children.sql
@@ -59,12 +59,12 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (351300, 9, 2, 0, 0, 0, 100, 0, 2000, 2000, 0, 0, 84, 0, 0, 0, 0, 0, 0, 11, 3511, 15, 0, 0, 0, 0, 0, 'Simple Talk - Steven'),
 (351300, 9, 3, 0, 0, 0, 100, 0, 6000, 6000, 0, 0, 1, 2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Speech');
 
-DELETE FROM `smart_scripts` WHERE (`source_type` = 9 AND `entryorguid` = 351300);
+DELETE FROM `smart_scripts` WHERE (`source_type` = 9 AND `entryorguid` = 351301);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(351300, 9, 0, 0, 0, 0, 100, 0, 2000, 2000, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Speech'),
-(351300, 9, 1, 0, 0, 0, 100, 0, 8000, 8000, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Speech'),
-(351300, 9, 2, 0, 0, 0, 100, 0, 2000, 2000, 0, 0, 84, 0, 0, 0, 0, 0, 0, 11, 3511, 15, 0, 0, 0, 0, 0, 'Simple Talk - Steven'),
-(351300, 9, 3, 0, 0, 0, 100, 0, 6000, 6000, 0, 0, 1, 2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Speech');
+(351301, 9, 0, 0, 0, 0, 100, 0, 2000, 2000, 0, 0, 1, 3, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Speech'),
+(351301, 9, 1, 0, 0, 0, 100, 0, 8000, 8000, 0, 0, 1, 4, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Speech'),
+(351301, 9, 2, 0, 0, 0, 100, 0, 2000, 2000, 0, 0, 84, 3, 0, 0, 0, 0, 0, 11, 3511, 15, 0, 0, 0, 0, 0, 'Simple Talk - Steven'),
+(351301, 9, 3, 0, 0, 0, 100, 0, 6000, 6000, 0, 0, 1, 5, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Speech');
 
 DELETE FROM `smart_scripts` WHERE (`source_type` = 9 AND `entryorguid` = 351302);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
